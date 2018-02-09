5f21ccda37b427936eab59ac4a6e89f1b1799a98
ahmadsq95 <ahmad.alqurashi95@gmail.com>
2018-02-09 18:40:17 +0300

    adding connection to server put there an error

--- app/src/main/java/s/ahmadsq/myapplication/loginActivity.java
+++ app/src/main/java/s/ahmadsq/myapplication/loginActivity.java
@@ -1,5 +1,6 @@
 package s.ahmadsq.myapplication;
 
+import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class loginActivity extends AppCompatActivity {
 
     @Override
@@ -15,7 +25,19 @@ public class loginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        
+        RequestQueue queue= Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.POST, "10.0.2.2", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) throws JSONException {
+                Toast.makeText(getBaseContext(),response,Toast.LENGTH_SHORT).show();
+
+
+                JSONObject jsonObject = new JSONObject(response);
+                String XXXX = jsonObject.getString("login.php");
+
+
+            }
+        });
         Button loginButton =(Button) findViewById(R.id.loginButton);
         final EditText usernameTextFeild = (EditText) findViewById(R.id.usernameEditText);
         EditText passwordTextFeild = (EditText) findViewById(R.id.passwordEditText);
